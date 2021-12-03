809cfdfaacc59dc5a42f4d3f072f52f524436bf1
TanyaHendricks <bob4robert@gmail.com>
2021-12-03 20:34:58 +0200

    added document page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [    
     path('', views.portfolio, name="portfolio" ),
+    path('document/<str:pk>', views.viewDocument, name="document" ),
 ]
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def portfolio(request):
     return render(request, 'portfolio/portfolio.html')
+
+def viewDocument(request, pk):
+    return render(request, 'portfolio/document.html')
