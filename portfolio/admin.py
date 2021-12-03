7a9d2f400e1ddadf12253d7afc5157e7d4dad773
TanyaHendricks <bob4robert@gmail.com>
2021-12-03 22:25:47 +0200

    migated db

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Interest
+from .models import Website
+from .models import Institution
+from .models import DocumentType
+from .models import Experience
+from .models import Document
 
 # Register your models here.
+
+admin.site.register(Interest)
+admin.site.register(Website)
+admin.site.register(Institution)
+admin.site.register(DocumentType)
+admin.site.register(Experience)
+admin.site.register(Document)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+class Interest(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    description = models.TextField(max_length=300, null=False, blank=False)
+    def _str_(self):
+        return self.title
+
+class Website(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    image = models.ImageField(null=False, blank=False)
+    url = models.URLField(null=False, blank=False)
+    def _str_(self):
+        return self.title
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    description = models.TextField(max_length=300, null=False, blank=False)
+    logo = models.ImageField(null=True, blank=True)
+    website = models.ForeignKey(Website, on_delete=models.RESTRICT, null=True, blank=True)
+    def _str_(self):
+        return self.name 
+
+class DocumentType(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    description = models.TextField(max_length=300, null=False, blank=False)
+    def _str_(self):
+        return self.name
+
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    institution = models.ForeignKey(Institution, on_delete=models.RESTRICT)
+    commenced = models.DateField()
+    completed = models.DateField()
+    description = models.TextField(max_length=1000, null=False, blank=False)
+    def _str_(self):
+        return self.title
+
+class Document(models.Model):    
+    document_type = models.ForeignKey(DocumentType, on_delete=models.RESTRICT)
+    title = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    issued = models.DateField()
+    issuer = models.ForeignKey(Institution, on_delete=models.RESTRICT)
+    experience = models.ForeignKey(Experience, on_delete=models.RESTRICT, null=False, blank=False)
+    image = models.ImageField(null=False, blank=False)
+    def _str_(self):
+        return self.title  
+
+
+
