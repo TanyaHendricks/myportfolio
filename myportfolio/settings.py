3774901c8b0b6585c34fac1360281dc5821ae8b7
TanyaHendricks <bob4robert@gmail.com>
2022-01-17 14:09:58 +0200

    Debug True Test

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mqcwb6w4r6=_h!kd9zjqa$ks8)1fj6#eqx+4*n-(k+99lo01p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['resumeet.herokuapp.com', '127.0.0.1']
