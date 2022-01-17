a4f44a625b7f24dd0ac5a14835afc8655750b907
TanyaHendricks <bob4robert@gmail.com>
2022-01-17 14:02:44 +0200

    Modified Staticfiles

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -125,17 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'static/image'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
